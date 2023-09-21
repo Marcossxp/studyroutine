30848a2455dc1b9312ede6c86808f58eb4d59369
Marcos <marcosaurelio2417@gmail.com>
2023-09-21 01:46:50 -0400

    perdão

--- lib/paginas/Inicio.dart
+++ lib/paginas/Inicio.dart
@@ -7,8 +7,8 @@ Container createGradientContainer(Widget child) {
     decoration: BoxDecoration(
       gradient: LinearGradient(
         colors: [Cores.pretogradiente, Cores.azulgradiente],
+        ),
       ),
-    ),
     child: child,
   );
 }
@@ -28,172 +28,172 @@ class Inicio extends StatelessWidget {
               color: Colors.white,
             ),
           ),
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          flexibleSpace: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Cores.pretogradiente, Cores.azulgradiente],
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              flexibleSpace: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Cores.pretogradiente, Cores.azulgradiente],
+                  ),
+                ),
               ),
-            ),
-          ),
-          leading: IconButton(
-            icon: Icon(
-              Icons.menu,
-              color: Colors.white,
-            ),
-            onPressed: () {},
-          ),
-        ),
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Cores.pretogradiente, Cores.azulgradiente],
-            ),
-          ),
-          child: Center(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 150.0),
-              child: SingleChildScrollView(
-                child: Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [Cores.pretogradiente, Cores.azulgradiente],
+              leading: IconButton(
+                icon: Icon(
+                  Icons.menu,
+                  color: Colors.white,
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+              body: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Cores.pretogradiente, Cores.azulgradiente],
+                  ),
+                ),
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 150.0),
+                    child: SingleChildScrollView(
+                    child: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [Cores.pretogradiente, Cores.azulgradiente],
+                          ),
+                          borderRadius: BorderRadius.circular(10.0),
+                          boxShadow: [
+                          BoxShadow(
+                            color: Cores.azulgradiente,
+                            offset: Offset(0, 2),
+                            blurRadius: 4.0,
+                          ),
+                        ],
+                      ),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  'Matéria do Dia',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                          Container(
+                            height: 2.0,
+                            color: Cores.azulgradiente,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Nome da disciplina/conteudo',
+                                hintStyle: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Nome da disciplina/conteudo',
+                                hintStyle: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Nome da disciplina/conteudo',
+                                hintStyle: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Nome da disciplina/conteudo',
+                                hintStyle: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                    borderRadius: BorderRadius.circular(10.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Cores.azulgradiente,
-                        offset: Offset(0, 2),
-                        blurRadius: 4.0,
-                      ),
-                    ],
-                  ),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text(
-                          'Matéria do Dia',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: 2.0,
-                        color: Cores.azulgradiente,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            hintText: 'Nome da disciplina/conteudo',
-                            hintStyle: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            hintText: 'Nome da disciplina/conteudo',
-                            hintStyle: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            hintText: 'Nome da disciplina/conteudo',
-                            hintStyle: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            hintText: 'Nome da disciplina/conteudo',
-                            hintStyle: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ],
                   ),
                 ),
               ),
             ),
-          ),
-        ),
-        bottomNavigationBar: Container(
-          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-          color: Colors.transparent,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              createGradientContainer(
-                Text(
-                  'Biblioteca',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
+                          bottomNavigationBar: Container(
+                            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                            color: Colors.transparent,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                          createGradientContainer(
+                            Text(
+                              'Biblioteca',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          createGradientContainer(
+                            Text(
+                              'Início',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          createGradientContainer(
+                            Text(
+                              'Estudar',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          createGradientContainer(
+                            Text(
+                              'Calendário',
+                              style: TextStyle(
+                                color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              createGradientContainer(
-                Text(
-                  'Início',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              createGradientContainer(
-                Text(
-                  'Estudar',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              createGradientContainer(
-                Text(
-                  'Calendário',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.transparent,
-      ),
+            ),
+                          theme: ThemeData(
+                          scaffoldBackgroundColor: Colors.transparent,
+                          ),
     );
   }
 }
